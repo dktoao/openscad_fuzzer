be8eb98ecc83cc2de2fc2f5c74d9485e8bf6ed5c
daniel.kuntz <dkuntz417@gmail.com>
2013-04-23 19:51:21 -0600

    Add logging capability and run 10000 trials

--- openscadfuzzer.py
+++ openscadfuzzer.py
@@ -14,8 +14,8 @@ app = "/usr/bin/openscad"
 fuzzy_file = "fuzzy.scad"
 
 # Test parameters
-fuzz_factor = 250
-num_tests = 10
+fuzz_factor = 100
+num_tests = 10000
 
 ############################## end configuration #############################
 
@@ -23,10 +23,25 @@ import math
 import random
 import string
 import subprocess
+import sqlite3
+
+# connect to a database for logging code runs
+log_db = sqlite3.connect('openscad_fuzzlog.sqlite')
+log_cur = log_db.cursor()
+
+log_cur.execute('''CREATE TABLE IF NOT EXISTS log (
+                   id INTEGER PRIMARY KEY,
+                   time_stamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                   error_code INTEGER,
+                   error_msg TEXT,
+                   input_file BLOB)''')
+
+log_db.commit()
 
 # outer test loop
 for n in range(num_tests):
     file_choice = random.choice(file_list)
+    print "Fuzzing run #{0}: {1}".format(n, file_choice)
 
     buf = bytearray(open(file_choice, 'rb').read())
 
@@ -42,8 +57,26 @@ for n in range(num_tests):
     open(fuzzy_file, 'wb').write(buf)
 
     # open process and start logging errors
-    process = subprocess.Popen([app, "-o test{0}.stl".format(n), fuzzy_file])
+    process = subprocess.Popen([app, "-o test.stl", fuzzy_file],
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    crashed = process.wait()
+    out, err_msg = process.communicate()
+    crashed = process.returncode
 
-    print crashed
+    # make sure that the error code is valid utf-8
+    try:
+        err_msg = unicode(err_msg)
+    except UnicodeDecodeError:
+        err_msg = "Error msg not valid utf-8"
+
+    if crashed < 0:
+        log_cur.execute('''INSERT INTO log (error_code, input_file, error_msg)
+                           VALUES (?,?,?)''', [crashed, buffer(buf),
+                           err_msg.decode('utf-8')])
+    else:
+        log_cur.execute('''INSERT INTO log (error_code, error_msg)
+                           VALUES (?,?)''', [crashed, err_msg.decode('utf-8')])
+    if n%100 == 0:
+        log_db.commit()
+
+log_db.commit()
